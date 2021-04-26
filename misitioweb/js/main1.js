d25b77f8eb8ad35fd9bf00c9df1807309b4688d4
kevinpolari <74829806+kevinpolari@users.noreply.github.com>
2021-04-26 04:38:57 -0300

    Add files via upload

---
+++ misitioweb/js/main1.js
@@ -0,0 +1,71 @@
+// Variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true ;
+
+function menus() {
+    let desplazamiento_actualizado = window.pageYOffset;
+
+    if(desplazamiento_actualizado <= 600){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        abrir.style.color = '#000';
+    }
+}
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+
+window.addEventListener('click', function(e){
+    if(cerrado==false){
+        console.log(e.target);
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hideen';
+            cerrado = true;
+        }
+    }
+})
+
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('widht');
+    }
+});
+
+abrir.addEventListener('click', function(){
+    apertura();
+});
